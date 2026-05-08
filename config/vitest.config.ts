1d5970c57196747434f8ff2a4bc5d941cac4f80f
frontalspy <frontalspy@gmail.com>
2026-05-08 21:35:29 +1000

    fixes type issues with jest-dom

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" />
+import "@testing-library/jest-dom/vitest";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     setupFiles: ["./src/app/setup-test.ts"],
-    include: ["**/*.spec.tsx"],
+    include: ["**/*.spec.{ts,tsx}"],
     globals: true,
     environment: "jsdom",
   },
